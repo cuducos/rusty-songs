1cfbf1c5ae01b6701a0d89b0186862726249dbe2
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2023-09-18 17:50:21 -0400

    Solves using enum (instead of dyn dispatch)

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,11 @@ impl Vinyl {
 
 impl Album for Vinyl {
     fn display_name(&self) -> String {
-        format!("{} by {}", self.name, self.artist)
+        format!("[Vinyl] {} by {}", self.name, self.artist)
     }
 
     fn play(&self) {
-        println!(
-            "[Vinyl] Playing {} (Side {})",
-            self.display_name(),
-            self.side
-        );
+        println!("Playing {} (Side {})", self.display_name(), self.side);
     }
 }
 
@@ -40,45 +36,41 @@ struct CompactDisc {
 
 impl Album for CompactDisc {
     fn display_name(&self) -> String {
-        format!("{} by {}", self.name, self.artist)
+        format!("[CD] {} by {}", self.name, self.artist)
     }
 
     fn play(&self) {
-        println!("[CD] Playing {}", self.display_name());
+        println!("Playing {}", self.display_name());
     }
 }
 
+enum Albums {
+    Vinyl(Vinyl),
+    CompactDisc(CompactDisc),
+}
+
 fn main() {
-    let lover = Box::new(Vinyl {
+    let lover = Albums::Vinyl(Vinyl {
         name: "Lover".to_string(),
         artist: "Taylor Swift".to_string(),
         side: "A".to_string(),
-    }) as Box<dyn Album>;
-    let midnights = Box::new(CompactDisc {
+    });
+    let midnights = Albums::CompactDisc(CompactDisc {
         name: "Midnights".to_string(),
         artist: "Taylor Swift".to_string(),
-    }) as Box<dyn Album>;
+    });
 
-    let collection: Vec<Box<dyn Album>> = vec![lover, midnights];
+    let collection: Vec<Albums> = vec![lover, midnights];
     for mut album in collection {
-        album.play();
-
-        // Trying to know whether it is a vinyl or not, to know if we need to
-        // flip it, fails with:
-        //
-        // error[E0599]: no method named `downcast` found for struct `Box<dyn Album>` in the current scope
-        //    --> src/main.rs:79:34
-        //    |
-        // 79 |         if let Ok(vinyl) = album.downcast::<Vinyl>() {
-        //    |                                  ^^^^^^^^ method not found in `Box<dyn Album>`
-        //    |
-        //    = note: the method was found for
-        //            - `Box<(dyn Any + 'static), A>`
-        //            - `Box<(dyn Any + Send + 'static), A>`
-        //            - `Box<(dyn Any + Send + Sync + 'static), A>`
-        if let Ok(vinyl) = album.downcast::<Vinyl>() {
-            vinyl.flip();
-            vinyl.play();
+        match album {
+            Albums::Vinyl(ref mut vinyl) => {
+                vinyl.play();
+                vinyl.flip();
+                vinyl.play();
+            }
+            Albums::CompactDisc(cd) => {
+                cd.play();
+            }
         }
     }
 }
